8b020a298b728cd1d2870fa60155a067d0a23101
alexandre artus <artusalexandre@yahoo.fr>
2023-02-27 17:13:09 +0100

    Create a new model human

---
+++ db/migrate/20230227160849_create_humen.rb
@@ -0,0 +1,13 @@
+class CreateHumen < ActiveRecord::Migration[7.0]
+  def change
+    create_table :humen do |t|
+      t.string :name
+      t.text :description
+      t.integer :price
+      t.string :category
+      t.references :dog, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

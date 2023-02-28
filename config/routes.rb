6f70b8c580067fac96806c66e93daf36f53873cc
Rebecca Sinai <rebeccahsinai@gmail.com>
2023-02-28 15:14:04 +0100

    updates to routes and html for views and partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :dogs
   root to: "pages#home"
-  resources :humans, only: %i[show new create] do
+  resources :humans, only: %i[index show new create] do
     resources :reservations, only: %i[new create]
   end
   resources :reservations, only: :destroy

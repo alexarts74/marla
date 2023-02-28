1b68353fccf4131b6d43dbdfa6c69fff6725dc63
alexandre artus <artusalexandre@yahoo.fr>
2023-02-28 11:35:29 +0100

    Create controller humans not finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
   devise_for :dogs
-  root to: "pages#home"
-  resources :dogs, only: %i[new create destroy] do
-    resources :humans, only: %i[index show new create] do
-      resources :reservation, only: %i[new create]
-    end
+  root to: "humans#index"
+  resources :humans, only: %i[show new create] do
+    resources :reservation, only: %i[new create]
   end
   resources :reservation, only: :destroy
   resources :humans, only: :destroy
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'seeding data'
+
+Human.destroy_all
+
+10.times do
+Human.create(
+  Faker::Name.name
+  Faker.commerce.price(min: number = 1, max: number = 1000
+)
+end

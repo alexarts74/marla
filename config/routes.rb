442e9b9ca7c657ae0e3f9ae26ec3ea9d56149e7a
Rebecca Sinai <rebeccahsinai@gmail.com>
2023-02-28 15:05:20 +0100

    updates to navbar, views, footer, index....

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :dogs
-  root to: "humans#index"
+  root to: "pages#home"
   resources :humans, only: %i[show new create] do
     resources :reservations, only: %i[new create]
   end

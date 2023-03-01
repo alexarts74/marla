d89fa89146192587f20c888154121bb3419abedc
alexandre artus <artusalexandre@yahoo.fr>
2023-03-01 16:07:49 +0100

    reservation controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :dogs
   root to: "humans#index"
   resources :humans, only: %i[show new create destroy] do
-    resources :reservations, only: %i[new create]
+    resources :reservations, only: %i[new create index]
   end
   resources :reservations, only: :destroy
   # resources :humans, only: :destroy

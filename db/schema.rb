23119207a8002fd4f483e2849b25fe07b1a626fa
alexandre artus <artusalexandre@yahoo.fr>
2023-03-02 13:29:06 +0100

    Def create controller humans et reparation de la base de données

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_163459) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_114825) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,7 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_163459) do
     t.string "name"
     t.text "description"
     t.integer "price"
-    t.string "category"
     t.bigint "dog_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

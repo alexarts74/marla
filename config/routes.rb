0c12b5c4c34075a6ab89d2ac2c0b75bfaebb4d7e
victor lombard <victor@lombard.fr>
2023-03-02 12:12:33 +0100

    finished method reservation controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   resources :humans, only: %i[show new create destroy] do
     resources :reservations, only: %i[new create index]
   end
-  resources :reservations, only: :destroy
+  resources :reservations, only: %i[destroy show]
+
   # resources :humans, only: :destroy
   get "/my-account", to: "dogs#my_account", as: "my-account"
 end

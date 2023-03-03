1af3e09b2be3d9bfbf7b0d349f13132174d73fa6
Rebecca Sinai <rebeccahsinai@gmail.com>
2023-03-03 11:19:53 +0100

    seeds creation first four seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,27 @@ require 'faker'
 
 puts 'seeding data...'
 
+# Category.destroy_all
+
+sporty = Category.create!(
+  name: "sporty"
+)
+feeds_well = Category.create!(
+  name: "feeds well"
+)
+cuddly = Category.create!(
+  name: "cuddly"
+)
+kind = Category.create!(
+  name: "kind"
+)
+great_frisbee_player = Category.create!(
+  name: "great frisbee player"
+)
+crazy = Category.create!(
+  name: "crazy"
+)
+
 Dog.destroy_all
 
 dog = Dog.new(
@@ -17,37 +38,72 @@ dog.save!
 
 Human.destroy_all
 
-10.times do
-  human = Human.new(
-    name: Faker::Name.name,
-    description: Faker::Lorem.paragraph(sentence_count: 2),
-    price: Faker::Number.between(from: 30, to: 300),
-    dog: dog
-  )
-  human_photo = Cloudinary::Uploader.upload("app/assets/images/ryan2.jpeg")
-  human.photo.attach(io: URI.open(human_photo['url']), filename: "ryan2.jpeg", content_type: "image/jpeg")
-  human.save!
-end
 
-Category.destroy_all
+aubrey = Human.new(
+  name: "Aubrey",
+  description: "You could have his heart or you could share it like the last slice",
+  price: Faker::Number.between(from: 30, to: 300),
+  dog: dog,
+)
 
-Category.create!(
-  name: "sporty"
+aubrey.categories = [sporty, feeds_well]
+
+aubrey_photo = Cloudinary::Uploader.upload("app/assets/images/drake.jpeg")
+aubrey.photo.attach(io: URI.open(aubrey_photo['url']), filename: "drake.jpeg", content_type: "image/jpeg")
+
+aubrey.save!
+
+christina = Human.new(
+  name: "Christina",
+  description: "Genie in a bottle",
+  price: Faker::Number.between(from: 30, to: 300),
+  dog: dog,
 )
-Category.create!(
-  name: "feeds well"
+
+christina.categories = [cuddly, kind]
+
+christina_photo = Cloudinary::Uploader.upload("app/assets/images/christina.jpeg")
+christina.photo.attach(io: URI.open(christina_photo['url']), filename: "christina.jpeg", content_type: "image/jpeg")
+
+christina.save!
+
+bow_wow = Human.new(
+  name: "Bow Wow",
+  description: "Bow-wow-wow, yippie-yo, yippie-yay
+  Where my dogs at? Bark with me now",
+  price: Faker::Number.between(from: 30, to: 300),
+  dog: dog,
 )
-Category.create!(
-  name: "cuddly"
+
+bow_wow.categories = [great_frisbee_player, crazy]
+
+bow_wow_photo = Cloudinary::Uploader.upload("app/assets/images/lilbowwow.jpeg")
+bow_wow.photo.attach(io: URI.open(bow_wow_photo['url']), filename: "lilbowwow.jpeg", content_type: "image/jpeg")
+
+bow_wow.save!
+
+pitbull = Human.new(
+  name: "Pitbull",
+  description: "Haha it's Mr. 305 checkin' in for the remix - you know that 75 Street Brazil? Well, this year gon' be called Calle Ocho hahahaha",
+  price: Faker::Number.between(from: 30, to: 300),
+  dog: dog,
 )
-Category.create!(
-  name: "kind"
-)
-Category.create!(
-  name: "great frisbee player"
-)
-Category.create!(
-  name: "crazy"
-)
+
+pitbull.categories = [crazy]
+
+pitbull_photo = Cloudinary::Uploader.upload("app/assets/images/pitbull.jpeg")
+pitbull.photo.attach(io: URI.open(pitbull_photo['url']), filename: "pitbull.jpeg", content_type: "image/jpeg")
+
+pitbull.save!
+
+
+
+# Human.create!()
+
+# Human.create!()
+
+# Human.create!()
+
+
 
 puts 'seed ok'
